8ddae8c26bb927bb7b8ddacb3ec7aa9ee3fe6755
Shawn Wilkinson <me@super3.org>
2015-04-02 10:25:32 -0400

    Improvements to Setup and File Cache

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup
 
-setup(name='Uploading', version='1.0',
-      description='Code example demonstrating how to upload a file using Flask',
-      author='Miguel Molina', author_email='info@runnable.com',
-      url='http://www.runnable.com/',
+setup(name='uploader', version='1.0',
+      description='rustless and cryptographically audited public file server.',
+      author='Shawn Wilkinson', author_email='shawn+hashserv@storj.io',
+      url='http://storj.io',
 
       #  Uncomment one or more lines below in the install_requires section
       #  for the specific client drivers/modules your application needs.
