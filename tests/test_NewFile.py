102b301115252ed8b7363b471204ed21ab3731b9
Shawn Wilkinson <me@super3.org>
2015-04-17 00:02:39 -0400

    Unit Tests Fixed and Working

--- tests/test_NewFile.py
+++ tests/test_NewFile.py
@@ -4,20 +4,21 @@ from hashserv.NewFile import NewFile
 class NewFile_Test(unittest.TestCase):
 
 	def test_success(self):
-		newfile = NewFile('testing/random.txt', True)
+		newfile = NewFile('tests/random.txt', True)
 
 		# Testing success
 		ch = '969a5145d8b6ed3228e493fb7a66d07d3253be89af6094cf510986ebcd7d2fbd'
-		newfile.process('testing/')
+		newfile.process('tests/')
 		self.assertTrue(newfile.processed)
 		self.assertTrue(newfile.hash == ch)
 
 	def test_fail(self):
-		newfile = NewFile('testing/nothere.txt', True)
+		newfile = NewFile('tests/nothere.txt', True)
 
 		# Testing fail
-		newfile.process('testing/')
+		newfile.process('tests/')
 		self.assertTrue(not newfile.processed)
+
 
 if __name__ == '__main__':
 	suite = unittest.TestLoader().loadTestsFromTestCase(TestNewFile)
