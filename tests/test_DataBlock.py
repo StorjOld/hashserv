727ba1783cf99e8346945a98e7a9d5a2c8d11a21
Shawn Wilkinson <me@super3.org>
2015-04-23 23:02:02 -0400

    More Test Coverage

--- tests/test_DataBlock.py
+++ tests/test_DataBlock.py
@@ -9,11 +9,35 @@ class DataBlockTest(unittest.TestCase):
     def test_create_data_block(self):
         block = DataBlock(1)
 
-        block.merkle_tree.add_hash(sha256("test"))
-        block.merkle_tree.add_hash(sha256("test2"))
-        block.merkle_tree.add_hash(sha256("test3"))
+        block.add_hash(sha256("test"))
+        block.close()
 
+        ans = '9f86d081884c7d659a2feaa0c55ad015a3bf4f1b2b0b822cd15d6c15b0f00a08'
+        self.assertEqual(block.merkle_root(), ans)
+
+    def test_data_block_json(self):
+        block = DataBlock(1)
+
+        block.add_hash(sha256("test"))
+        block.add_hash(sha256("test2"))
+        block.add_hash(sha256("test3"))
+
+        json_data = block.to_json()
+
+        self.assertEqual(json_data['block_num'], 1)
+        self.assertFalse(json_data['closed'])
+        self.assertEqual(json_data['merkle_root'], None)
+        self.assertEqual(json_data['tx_id'], None)
+        self.assertEqual(len(json_data['leaves']), 3)
+
+        # close block and try again
+        block.close()
+
+        json_data = block.to_json()
         ans = 'd49e815a91a26d399f8c2fba429e6ef7e472e54b6eb1e04341d207eee219f6c0'
 
-        block.close()
-        self.assertEqual(block.merkle_root(), ans)
+        self.assertEqual(json_data['block_num'], 1)
+        self.assertTrue(json_data['closed'])
+        self.assertEqual(json_data['merkle_root'], ans)
+        self.assertEqual(json_data['tx_id'], None)
+        self.assertEqual(len(json_data['leaves']), 3)
