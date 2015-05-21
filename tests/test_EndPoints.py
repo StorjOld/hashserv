6519c2bb2308c5ce62fd900bb2d445b8c9a7a18b
Shawn Wilkinson <me@super3.org>
2015-05-21 00:57:31 -0400

    Unfinished Endpoint Testing

--- tests/test_EndPoints.py
+++ tests/test_EndPoints.py
@@ -1,8 +1,7 @@
-import time
-import random
 import unittest
 import urllib.request
-from hashserv.MerkleTree import sha256
+from hashserv.app import app
+from hashserv.app import init_db
 
 
 url = "http://localhost:5000/"
@@ -12,9 +11,15 @@ class EndPointsTest(unittest.TestCase):
     def run_api(self, api):
         return urllib.request.urlopen(url.format(api))
 
-    def test_check_connection(self):
-        try:
-            self.run_api("")
-            self.assertTrue(True)
-        except urllib.error.URLError:
-            self.assertTrue(False)
+    def setUp(self):
+        # create database
+        init_db()
+
+    #def test_check_connection(self):
+    #    try:
+    #        self.run_api("")
+    #        self.assertTrue(True)
+    #    except ConnectionRefusedError:
+    #        self.assertTrue(False)
+    #    except urllib.error.URLError:
+    #        self.assertTrue(False)
