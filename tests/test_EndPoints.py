4658bd66f9719ac97e8926d129f82b50c643a0e7
Shawn Wilkinson <me@super3.org>
2015-05-21 00:35:50 -0400

    Start Endpoint Testing

--- tests/test_EndPoints.py
+++ tests/test_EndPoints.py
@@ -1,6 +1,20 @@
+import time
+import random
 import unittest
+import urllib.request
+from hashserv.MerkleTree import sha256
+
+
+url = "http://localhost:5000/"
 
 
 class EndPointsTest(unittest.TestCase):
-    def simple_test(self):
-        self.assertTrue(False)
+    def run_api(self, api):
+        return urllib.request.urlopen(url.format(api))
+
+    def test_check_connection(self):
+        try:
+            self.run_api("")
+            self.assertTrue(True)
+        except urllib.error.URLError:
+            self.assertTrue(False)
