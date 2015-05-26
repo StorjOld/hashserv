d047e9e4ec315a2cf1799a28af6099d4018ee433
Shawn Wilkinson <me@super3.org>
2015-05-26 17:51:44 -0400

    PEP8 Fix

--- hashserv/config.py
+++ hashserv/config.py
@@ -1,5 +1,5 @@
-DATABASE = "hashserv.db" # path to database
+DATABASE = "hashserv.db"  # path to database
 PRIVATE_KEYS = ["cMhtifPTA8s8demWt73kBJmdsdhchSAegoEei91gz3q6HPr6H6XV"]
-CHANGE_ADDRESS = "mhmHpLGFK5tVaFda1BJkzVjCKasWvACjNr" # from private key
+CHANGE_ADDRESS = "mhmHpLGFK5tVaFda1BJkzVjCKasWvACjNr"  # from private key
 TESTNET = True
-FEE = 10000 # satoshis 0.0001btc
+FEE = 10000  # satoshis 0.0001btc
