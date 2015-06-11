1cfef1d0236fc7e95e9bf0c9e73f6f513ed7e706
Shawn Wilkinson <me@super3.org>
2015-06-10 22:12:56 -0400

    Delete Old Stuff

--- tests/test_DataHash.py
+++ tests/test_DataHash.py
@@ -1,12 +1,23 @@
+import unittest
 from flask import Flask
 from hashserv.DataHash import db
-from flask.ext.testing import TestCase
 from hashserv.DataHash import DataHash
-from hashserv.DataBlock import DataBlock
 
 
-class DataHashTest(TestCase):
+class DataHashTest(unittest.TestCase):
 
+    # start up
+    def setUp(self):
+        db.create_all()
+        #block = DataBlock(1, 1)
+        #db.session.add(block)
+        #db.session.commit()
+
+    def tearDown(self):
+        db.session.remove()
+        #db.drop_all()
+
+    # test cases
     def test_valid_sha256(self):
         valid_hash = '9f86d081884c7d659a2feaa0c55ad015a3bf4f1b2b0b822cd15d6c15b0f00a08'
         data = DataHash(valid_hash)
@@ -15,21 +26,6 @@ class DataHashTest(TestCase):
         invalid_hash = 'notarealhash'
         data = DataHash(invalid_hash)
         self.assertFalse(data.is_sha256())
-
-    def create_app(self):
-        app = Flask(__name__)
-        app.config['TESTING'] = True
-        return app
-
-    def setUp(self):
-        db.create_all()
-        block = DataBlock(1, 1)
-        db.session.add(block)
-        db.session.commit()
-
-    def tearDown(self):
-        db.session.remove()
-        db.drop_all()
 
     def test_insertion(self):
         valid_hash = '9f86d081884c7d659a2feaa0c55ad015a3bf4f1b2b0b822cd15d6c15b0f00a08'
